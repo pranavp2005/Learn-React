68c1a44ebd639a0feadd590b15559c0ee3594c08
pranavp2005 <f20180686@goa.bits-pilani.ac.in>
2021-12-07 10:06:38 +0530

    Class based components

---
+++ Udemy_React_the_complete_guide/Class_Based_Components/01-starting-project/src/components/User.js
@@ -0,0 +1,13 @@
+import classes from './User.module.css';
+
+
+class User  {
+  render() {
+    
+  }
+}
+const User = (props) => {
+  return <li className={classes.user}>{props.name}</li>;
+};
+
+export default User;
